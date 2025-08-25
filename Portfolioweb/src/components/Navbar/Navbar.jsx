956d01c655321e465e05da2478427225a636e43f
suyogzz78 <sakurachhetri97@gmail.com>
2025-08-25 23:05:59 +0545

    Navbar

--- Portfolioweb/src/components/Navbar/Navbar.jsx
+++ Portfolioweb/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { TfiMenu } from "react-icons/tfi";
+import { FiX } from "react-icons/fi";
+import { FaGithubSquare } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className='bg-transparent w-full fixed top-0 left-0 z-50'>
+      <div className='text-white flex justify-between items-center '>
+      <div className='text-lg font-bold cursor-pointer'>
+        <span className='text-blue-900'>&lt;</span>
+         <span className='text-white'>Suyog</span>
+          <span className='text-blue-900'>/</span>
+           <span className='text-white'>Gautam</span>
+            <span className='text-blue-900'>&gt;</span>
+      </div>
+       </div>
+      
+    </nav>
   )
 }
 
