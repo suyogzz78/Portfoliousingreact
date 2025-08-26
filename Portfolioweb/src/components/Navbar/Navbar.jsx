1edbbd6e091db73840d7bf509dec6c53f17976f5
suyogzz78 <sakurachhetri97@gmail.com>
2025-08-26 14:23:07 +0545

    Desktopnav

--- Portfolioweb/src/components/Navbar/Navbar.jsx
+++ Portfolioweb/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React,{useState}from "react";
+import React,{useEffect, useState}from "react";
 import { TfiMenu } from "react-icons/tfi";
 import { FiX } from "react-icons/fi";
 import { FaGithubSquare } from "react-icons/fa";
@@ -7,6 +7,21 @@ import { FaLinkedin } from "react-icons/fa";
 function Navbar() {
   const [isopen, setIsOpen] = useState(false);
   const [activesection, setActiveSection] = useState("");
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handlescroll = () => {
+
+      setIsScrolled(window.scrollY > 50);
+    };
+    window.addEventListener("scroll", handlescroll);
+    return () => {
+      window.removeEventListener("scroll", handlescroll);
+    }
+
+  }, []);
+
+
   const handlemenuitemclick = (e) => {
     setActiveSection(e);
     setIsOpen(false);
@@ -21,7 +36,8 @@ function Navbar() {
   
   ];
   return (
-    <nav className="bg-transparent w-full fixed top-0 left-0 z-50">
+    <nav className={` w-full fixed top-0 left-0 z-50 transition duration-300 px-[7vw] md:px[7vw]  lg:px[20vw]
+     ${isScrolled ?"bg-[#0f172a] bg-opacity-40 backdrop-blur-md shadow-md":"bg-transparent" }`}>
       <div className="text-white flex justify-between items-center ">
         <div className="text-2xl font-bold cursor-pointer">
           <span className="text-blue-900">&lt;</span>
@@ -42,6 +58,24 @@ function Navbar() {
           </li>
         ))}
         </ul>
+        <div className="hidden md:flex space-x-4 text-2xl text-gray-500">
+          <a
+            href="https://github.com/suyogzz78"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-blue-400"
+          >
+            <FaGithubSquare size={25} />
+          </a>
+               <a
+            href="https://www.linkedin.com/in/suyog-gautam-273b40315/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-blue-400"
+          >
+            <FaLinkedin size={25} />
+          </a>
+          </div>
         </div>
     </nav>
   );
