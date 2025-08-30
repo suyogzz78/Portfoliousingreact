96e2f32267a16ac8befa94393169d4a9f23b1f5b
suyogzz78 <sakurachhetri97@gmail.com>
2025-08-30 19:15:52 +0545

    update

--- Portfolioweb/tailwind.config.js
+++ Portfolioweb/tailwind.config.js
@@ -1,13 +1,22 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        blob: {
+          "0%, 100%": { transform: "translate(-50%, -50%) scale(1)" },
+          "33%": { transform: "translate(-50%, -50%) scale(1.2)" },
+          "66%": { transform: "translate(-50%, -50%) scale(0.8)" },
+        },
+      },
+      animation: {
+        blob: "blob 6s infinite ease-in-out",
+      },
+    },
   },
   plugins: [],
-}
-
+};
