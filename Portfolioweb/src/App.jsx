16a199395bca0d4324bc82b9087e98f569c117c4
suyogzz78 <sakurachhetri97@gmail.com>
2025-08-30 19:14:15 +0545

    Update

--- Portfolioweb/src/App.jsx
+++ Portfolioweb/src/App.jsx
@@ -1,33 +1,33 @@
-import React, { useState } from 'react'
-import Navbar from './components/Navbar/Navbar'
-import About from './components/About/About'
-import Experience from './components/Experience/Experience'
-import Skills from './components/Skills/Skills'
-import Work from './components/Work/Work'
-import Contact from './components/Contact/Contact'
-import Footer from './components/Footer/Footer'
-import Education from './components/Education/Education'
+// src/App.jsx
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import About from "./components/About/About";
+import Skills from "./components/Skills/Skills";
+import Experience from "./components/Experience/Experience";
+import Work from "./components/Work/Work";
+import Education from "./components/Education/Education";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+import BlurBlob from "./components/BlurBlob";
 
-function App() {
+const App = () => {
   return (
-    <>
-      <div className="bg-[#0f172a]">
-        <div className='absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]'></div>
-        <div className='relative pt-20'>
-          <Navbar />
-          <About />
-          <Experience />
-          <Skills />
-          <Education />
-          <Work />
-          <Contact />
-          <Footer />
-      
+    <div className="relative bg-[#0a0922] min-h-screen overflow-hidden">
+  <BlurBlob position={{ top: '50%', left: '40%' }} size={{ width: '30%', height: '35%' }} />
+  <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
+  <div className="relative pt-20 z-10">
+    <Navbar />
+    <About />
+    <Skills />
+    <Experience />
+    <Work />
+    <Education />
+    <Contact />
+    <Footer />
+  </div>
+</div>
 
-        </div>
-      </div>
-    </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
