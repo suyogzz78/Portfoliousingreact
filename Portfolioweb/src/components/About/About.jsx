d547d87d0a2043f9c8b5a344cbb4dd7ef65ec1ca
suyogzz78 <sakurachhetri97@gmail.com>
2025-08-31 07:45:57 +0545

    tiltcomponentupdate

--- Portfolioweb/src/components/About/About.jsx
+++ Portfolioweb/src/components/About/About.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
 import { Typewriter } from "react-simple-typewriter";
+import Tilt from 'react-parallax-tilt';
 
+import profile1 from '../../assets/profile1.jpg'
 function About() {
 
 
@@ -18,7 +20,7 @@ function About() {
           <h2 className='text-4xl md:text-4xl lg:text-5xl font-bold text-white mb-4 leading-tight'>
             Suyog Gautam
           </h2>
-          <h3 className='text-xl sm:text-2xl md:3xl font-semibold mb-3 text-[#09094a]'>
+          <h3 className='text-xl sm:text-2xl md:3xl font-semibold mb-3 text-[#08083f]'>
             <span className='text-white'>  
                I am a
 
@@ -41,7 +43,38 @@ function About() {
               I am a passionate Full Stack Developer with expertise in React, Node.js, and MongoDB. I love building dynamic and responsive web applications that provide seamless user experiences. With a strong foundation in both front-end and back-end technologies, I enjoy tackling complex problems and turning ideas into reality through code.
                </p>
           </h3>
+          <a 
+          href='https://drive.google.com/file/d/1ZVYJHk3jv7mX4X1F8F2z5K6Z8Y9X2A3B/view?usp=sharing'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105'
+              style={{
+                background: 'linear-gradient(90deg, #87CEEB, [#09094a])',
+                boxShadow: '0 0 2px #87CEEB, 0 0 2px #87CEEB, 0 0 40px #87CEEB',
+              }}
+         >Download CV</a>
         </div>
+        <div className='md:w-1/2 flex justify-center md:justify-end'>
+       <div className='md:w-1/2 flex justify-center md:justify-end'>
+        <Tilt
+          className='w-48 h-48 border-5 rounded-full md:h-[30rem] md:w-[30rem] sm:w-64 sm:h-64 border-blue-700'  
+          tiltMaxAngleX={15}
+          tiltMaxAngleY={15}
+          perspective={1000}
+          scale={1.1}
+          transitionSpeed={1000}
+          gyroscope={true}
+        >  
+          <img 
+            src={profile1}
+            alt='Suyog Gautam'
+            className='w-full h-full object-cover  rounded-full drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]'
+          />
+        </Tilt>
+      </div>
+        
+        </div>
+
         </div>
 
     </section>
