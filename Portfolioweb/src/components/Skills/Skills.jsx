3afd9bf18fdc12daba4eb96e0fe3570be646a3e4
Suyog Gautam <sakurachhetri97@gmail.com>
2025-09-04 21:26:51 +0545

    update

--- Portfolioweb/src/components/Skills/Skills.jsx
+++ Portfolioweb/src/components/Skills/Skills.jsx
@@ -6,7 +6,7 @@ function Skills() {
     id="skills"
     className='py-24 pb-24 px-[7vw] md:px-[7vw] lg:px-[20vw] mt-10 md:mt-20 bg-gradient-rainbow lg:mt-32'>
       <div className='text-center mb-12'>
-       <h2 className='text-3xl font-bold text-white sm:text-4xl'>SKILLS ISSUE</h2> 
+       <h2 className='text-3xl font-bold text-white sm:text-4xl'>SKILLS ISSUE what</h2> 
       </div>
     </section>
   )
