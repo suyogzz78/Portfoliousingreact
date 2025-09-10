6932b94df108087d03adee0fa66044f7abfc8bc7
Suyog Gautam <sakurachhetri97@gmail.com>
2025-09-10 22:21:19 +0545

    update

--- Portfolioweb/src/components/Skills/Skills.jsx
+++ Portfolioweb/src/components/Skills/Skills.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { SkillsInfo } from '../../constant'
+
 function Skills() {
   return (
     <section
-    id="skills"
-    className='py-24 pb-24 px-[7vw] md:px-[7vw] lg:px-[20vw] mt-10 md:mt-20 bg-gradient-rainbow lg:mt-32'>
-      <div className='text-center mb-12'>
-       <h2 className='text-3xl font-bold text-white sm:text-4xl'>SKILLS ISSUE what</h2> 
+      id="skills"
+      className="py-24 pb-24 px-[7vw] md:px-[7vw] lg:px-[20vw] mt-10 md:mt-20 
+      bg-gradient-to-r from-[#000428] via-[#0f172a] to-[#1e3a8a] lg:mt-32">
+      <div className="text-center mb-12">
+        <h2 className="text-3xl font-bold text-white sm:text-4xl">SKILLS</h2>
       </div>
     </section>
   )
 }
 
-export default Skills 
+export default Skills
