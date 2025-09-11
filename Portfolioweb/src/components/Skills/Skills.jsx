80d963caca5a27dee90f4e02db719558f6cb9d51
Suyog Gautam <sakurachhetri97@gmail.com>
2025-09-11 21:38:18 +0545

    update

--- Portfolioweb/src/components/Skills/Skills.jsx
+++ Portfolioweb/src/components/Skills/Skills.jsx
@@ -12,6 +12,14 @@ function Skills() {
         <div className="mt-4 w-24 h-1 bg-blue-900 mx-auto rounded-full"></div>
         <p className='mt-4 text-white'>These are the technologies I've worked with</p>
       </div>
+
+      <div className='flex flex-wrap justify-between gap-1 lg:gap-4 py-10'>
+        {SkillsInfo.map((category)=> (<div
+        key={category.title}
+        className='bg-gray-900 backdrop-blur-md '>
+
+          </div>))}
+        </div>
     </section>
   )
 }
