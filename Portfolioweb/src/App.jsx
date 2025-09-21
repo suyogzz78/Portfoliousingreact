6cef6fae292caa7c40c877b25e569dc1c553bf1e
Suyog Gautam <sakurachhetri97@gmail.com>
2025-09-21 15:13:24 +0545

    update

--- Portfolioweb/src/App.jsx
+++ Portfolioweb/src/App.jsx
@@ -13,7 +13,7 @@ import BlurBlob from "./components/BlurBlob";
 const App = () => {
   return (
     <div className="relative bg-[#070621] min-h-screen overflow-hidden">
-  <BlurBlob position={{ top: '30%', left: '40%' }} size={{ width: '25%', height: '20%' }} />
+  <BlurBlob position={{ top: '27%', left: '43%' }} size={{ width: '25%', height: '20%' }} />
   <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
   <div className="relative pt-20 z-10">
     <Navbar />
