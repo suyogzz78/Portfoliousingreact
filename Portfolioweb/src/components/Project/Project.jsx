1c178caf6d5c91bb6508a5c6609ed71bd9bc3173
Suyog Gautam <sakurachhetri97@gmail.com>
2026-01-06 22:42:18 +0545

    newcompoforpportfolio

---
+++ Portfolioweb/src/components/Project/Project.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { projects } from '../../constant'
+function Project() {
+  return (
+   <section id="project" className="py-12 pb-10 px:[12vw] lg:px-[20vw]   bg-gradient-to-r from-[#000428] via-[#0f172a] to-[#1e3a8a]">
+    <div className="text-center mb-10">
+      <div className='text-2xl sm:text-3xl md:text-4xl font-bold text-white'>PROJECTS</div>
+           <div className='w-28 h-1 bg-blue-900 mx-auto mt-3 rounded-lg'></div>
+      </div>
+
+      </section>
+  )
+}
+
+export default Project
