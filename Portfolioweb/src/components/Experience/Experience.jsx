d7d2289342227bb9b484935f2893514d0cc29ee5
Suyog Gautam <sakurachhetri97@gmail.com>
2026-01-10 22:02:54 +0545

    added some projects in portfolio

--- Portfolioweb/src/components/Experience/Experience.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Experience() {
-  return (
-    <div></div>
-  )
-}
-
-export default Experience
--- Portfolioweb/src/components/Project/Project.jsx
+++ Portfolioweb/src/components/Project/Project.jsx
@@ -4,7 +4,7 @@ function Project() {
   return (
     <section
       id="project"
-      className="py-12 pb-10 px:[12vw] lg:px-[20vw]   bg-gradient-to-r from-[#000428] via-[#0f172a] to-[#1e3a8a]"
+      className="py-12 pb-10 px:[12vw] lg:px-[20vw]  w-full bg-gradient-to-r from-[#000428] via-[#0f172a] to-[#1e3a8a]"
     >
       <div className="text-center mb-10">
         <div className="text-2xl sm:text-3xl md:text-4xl font-bold text-white">
@@ -20,22 +20,31 @@ function Project() {
         {projects.map((project) => (
           <div
             key={project.id}
-            className=" border-purple-400 bg-gray-300 cursor-pointer backdrop-blur-md shadow-2xl overflow-hidden hover:shadow-purple-600 hover:translate-y-2 transition-transform duration-300 rounded-lg w-full h-full"
+            className=" border-purple-500 bg-slate-900 cursor-pointer backdrop-blur-md shadow-2xl overflow-hidden border-2 hover:shadow-purple-600 hover:translate-y-2 transition-transform duration-300 rounded-lg w-full h-full"
           >
             <div className="p-5">
               <img
                 src={project.image}
                 alt={project.title}
-                className="w-full h-48 object-cover"
+                className="w-full h-48 object-cover"s
               />
             </div>
             <div className="p-5">
               <div>
-                <h3 className="text-xl font-bold mb-2 text-gray-800">
+                <h3 className="text-xl font-bold mb-2 text-white">
                   {project.title}
                 </h3>
-                <p className="text-gray-600 mb-4">{project.description}</p>
+                <p className="text-gray-600 mb-4 pt-5 line-clamp-3">{project.description}</p>
               </div>
+              <div className="mt-4">
+                {project.tags.map((tags, index) => (
+                  <div className="inline-block bg-purple-900 text-white text-xs px-3 py-1 rounded-full mr-2 mb-3">
+                    {tags}
+                  </div>
+                ))}
+               
+
+                </div>
             </div>
           </div>
         ))}
