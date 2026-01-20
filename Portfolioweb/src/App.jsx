9beb4ce0fc48942d921570c62b88048a14e2e437
Suyog Gautam <sakurachhetri97@gmail.com>
2026-01-20 16:55:53 +0545

    changesmadeincontact

--- Portfolioweb/src/App.jsx
+++ Portfolioweb/src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import About from "./components/About/About";
 import Skills from "./components/Skills/Skills";
-import Experience from "./components/Experience/Experience";
+import Project from "./components/Project/Project";
 import Work from "./components/Work/Work";
 import Education from "./components/Education/Education";
 import Contact from "./components/Contact/Contact";
@@ -19,7 +19,7 @@ const App = () => {
     <Navbar />
     <About />
     <Skills />
-    <Experience />  
+    <Project />  
     <Work />
     <Education />
     <Contact />
