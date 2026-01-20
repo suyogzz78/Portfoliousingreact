3227d50e43c8a3fc6aead7fbbcbd01a540486dc0
Suyog Gautam <sakurachhetri97@gmail.com>
2026-01-20 16:55:19 +0545

    contact:addeddifferfentidsusingemailjsalso used toast for notifications

--- Portfolioweb/src/components/Contact/Contact.jsx
+++ Portfolioweb/src/components/Contact/Contact.jsx
@@ -1,12 +1,58 @@
 import React from "react";
+import {ToastContainer,toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import emailjs from '@emailjs/browser';
 
 function Contact() {
+
+  const [msgsent,setmsgsent]=React.useState(false);
+  const formref=React.useRef();//form ref is used to access the form element directly without rerendering the component
+
+  const handlesubmit=(e)=>{
+    e.preventDefault();// it prevents the default behavior of the form submission which is to reload the page
+
+    emailjs.sendForm(
+    "service_u83yy4o",//service id
+    "template_zmgzcrj",//template id
+    formref.current,
+    "o-KDG23NzpIgoHlMo"//public key
+    ).then(()=>{
+      setmsgsent(true);
+      formref.current.reset();//reset the form after submission
+      toast.success("Message sent successfully!", {
+        position:"top-right",
+        autoClose:5000,
+        hideProgressBar:false,
+        closeOnClick:true,
+        pauseOnHover:true,
+        draggable:true,
+        progress:undefined
+      });
+    },(error)=>{
+      toast.error("Failed to send message. Please try again later.", {
+        position:"top-right",
+        autoClose:5000,
+        hideProgressBar:false,
+        closeOnClick:true,
+        pauseOnHover:true,
+        draggable:true,
+        progress:undefined
+      });
+
+
+
+
+
+  })
+
+  }
   return (
     <section
       id="contact"
       className="flex flex-col items-center justify-center py-20 px-[7vw] md:px-[12vw] lg:px-[16vw] bg-gradient-rainbow"
     >
       {/* intro section */}
+      <ToastContainer />
 
       <div className="text-center mb-16">
         <h2 className="text-4xl font-bold text-white">CONTACT</h2>
@@ -19,7 +65,9 @@ function Contact() {
         <h2 className="text-2xl font-semibold text-white text-center">
           Contact Me
         </h2>
-        <form className="p-7 mt-4 flex flex-col gap-5">
+        <form ref={formref}
+        onSubmit={handlesubmit}
+         className="p-7 mt-4 flex flex-col gap-5">
           <input
             type="email"
             name="user_email"
@@ -61,3 +109,4 @@ function Contact() {
 }
 
 export default Contact;
+ 
